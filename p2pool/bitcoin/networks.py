92ecd8c3603b77faca40c2fcf054d88970226957
Drew Conner <drew@connermail.com>
2013-12-20 08:42:07 -0500

    Add Dogecoin, add second option for Worldcoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -199,6 +199,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=1e8,
     ),
+#    worldcoin=math.Object(
+#         P2P_PREFIX='fbc0b6db'.decode('hex'),
+#         P2P_PORT=11081,
+#         ADDRESS_VERSION=73,
+#         RPC_PORT=11082,
+#         RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+#             'worldcoinaddress' in (yield bitcoind.rpc_help()) and
+#             not (yield bitcoind.rpc_getinfo())['testnet']
+#         )),
+#         SUBSIDY_FUNC=lambda height: 32*10000000 >> (height + 1)//2650000,
+#         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+#         BLOCK_PERIOD=15, # s targetspacing
+#         SYMBOL='WDC',
+#         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'worldcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/worldcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.worldcoin'), 'worldcoin.conf'),
+#         BLOCK_EXPLORER_URL_PREFIX='http://wdc.cryptocoinexplorer.com/block/',
+#         ADDRESS_EXPLORER_URL_PREFIX='http://wdc.cryptocoinexplorer.com/address/',
+#         TX_EXPLORER_URL_PREFIX='http://wdc.cryptocoinexplorer.com/transaction/',
+#         SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
+#         DUMB_SCRYPT_DIFF=2**16,
+#         DUST_THRESHOLD=1e8,
+#     ),
 
     feathercoin=math.Object(
         P2P_PREFIX='fbc0b6db'.decode('hex'),
@@ -222,6 +243,28 @@ nets = dict(
         DUST_THRESHOLD=0.03e8,
     ),
 
+    dogecoin=math.Object(
+        P2P_PREFIX='c0c0c0c0'.decode('hex'),
+        P2P_PORT=22556,
+        ADDRESS_VERSION=30,
+        RPC_PORT=22555,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'dogecoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 10000*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='DOGE',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DogeCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Dogecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dogecoin'), 'dogecoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://dogechain.info/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://dogechain.info/address/',
+        TX_EXPLORER_URL_PREFIX='http://dogechain.info/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
+    
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
